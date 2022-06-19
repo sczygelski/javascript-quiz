43cde8b0d5ffd35b7c3bf6d9876f4ad91844eb49
Sczygelski <avamariescz@gmail.com>
2022-06-19 14:10:48 -0500

    added starting function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 //variables
+var startit = document.getElementById('start')
 
 //trigger the button to start quiz
+startit.addEventListener('click', startQuiz)
+function startQuiz() {
+    console.log('started')
+}
 
 //Show questions
 
+
 //Save answers
+function answerchosen() {
+
+}
 
 //highscores
