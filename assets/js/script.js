71eec5823b55cc77e51bed4d04faaa6c9911a868
Sczygelski <avamariescz@gmail.com>
2022-06-20 20:28:33 -0500

    trying to figure out showing questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 //variables
 var startit = document.getElementById('start')
+var questioning = document.getElementById('questionscontainer')
+var answering = document.getElementById('answers')
+var allquestionsElement = document.getElementById('questionscontainer')
+var actualquestionElement = document.getElementById('actualquestion')
 
 //trigger the button to start quiz
 startit.addEventListener('click', startQuiz)
 function startQuiz() {
     console.log('started')
     startit.classList.add('hide')
+    questioning.classList.remove('hide')
+    answering.classList.remove('hide')
+    currentQuestion = 0 
 }
 
 //Show questions
-
+function beginquestions() {
+   allquestions.forEach((currentQuestion, questionNumber) => {
+       
+   })
+};
 
 //Save answers
 function answerchosen() {
@@ -17,3 +28,17 @@ function answerchosen() {
 }
 
 //highscores
+
+
+//questions
+var allquestions = [
+    {
+    question: 'What is my name?',
+    answers: [
+        {text: 'Ava', correct:true},
+        {text: 'Brad', correct:false},
+        {text: 'Chad', correct:false},
+        {text: 'Dad', correct:false}
+    ]
+    }
+]
