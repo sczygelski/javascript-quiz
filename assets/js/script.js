a11b5d1bae289c35c58d67867612a82d9a0badb7
Sczygelski <avamariescz@gmail.com>
2022-10-21 19:57:36 -0500

    reworking starting quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
 //variables
 var startit = document.getElementById("start");
 var questioning = document.getElementById("questionscontainer");
-var allquestionsElement = document.getElementById("questionscontainer");
 var answering = document.getElementById("answers");
 var questionindex = 0;
+var buttona = document.getElementById("a");
+var buttonb = document.getElementById("b");
+var buttonc = document.getElementById("c");
+var buttond = document.getElementById("d");
+
 
 //questions
 let allquestions = [
   {
     title: "What type of brackets show an arrey?",
-    a:"{}",
-    b:"()",
-    c:"<>",
-    d:"[]",
+    a: "{}",
+    b: "()",
+    c: "<>",
+    d: "[]",
     answerchosen: "d",
   },
   {
@@ -25,6 +29,8 @@ let allquestions = [
   },
 ];
 
+var finalquestion = allquestions.length;
+
 //trigger the button to start quiz
 startit.addEventListener("click", startQuiz);
 function startQuiz() {
@@ -32,22 +38,38 @@ function startQuiz() {
   startit.classList.add("hide");
   questioning.classList.remove("hide");
   answering.classList.remove("hide");
-  render(allquestions);
+  startQuizing();
 }
 
 //Show questions
-function render(questionindex) {
-    questionscontainer.innerhtml=''
-  for (let i = 0; i < allquestions.length; i++) {
-    const currentquestion = allquestions[questionindex].title;
-    const currentanswers = allquestions[questionindex].answers;
-    allquestions.textConect = currentquestion;
+function startQuizing() {
+  var currentQuestion = allquestions[questionindex];
+  questioning.innerHTML = currentQuestion.title;
+  buttona.innerHTML = currentQuestion.a;
+  buttonb.innerHTML = currentQuestion.b;
+  buttonc.innerHTML = currentQuestion.c;
+  buttond.innerHTML = currentQuestion.d;
+}
+
+//check answers
+function checkanswers(answer) {
+  correct = allquestions[currentQuestion].answerchosen;
+
+  if (answer === correct && currentQuestion !== finalquestion) {
+    currentQuestion++;
+    startQuizing();
+  } else if (answer !== correct && currentQuestion !== finalquestion) {
+    currentQuestion++;
+    startQuizing();
+  } else {
+    showscore();
   }
 }
 
-
+//show scores
+//function showscore()
 
 //Save answers
-function answerchosen() {}
+//function answerchosen()
 
 //highscores
